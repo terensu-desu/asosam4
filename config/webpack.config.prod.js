ca29caa93f042781731ea0610c00eee408770cf5
Terence <tmangram@gmail.com>
2017-08-05 08:37:22 +0900

    plugin addition for stability

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -320,6 +320,12 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.ProvidePlugin({
+      $: 'materialize-css/node_modules/jquery/dist/jquery',
+      jQuery: 'materialize-css/node_modules/jquery/dist/jquery',
+      'window.jQuery': 'materialize-css/node_modules/jquery/dist/jquery',
+      Hammer: 'hammerjs/hammer'
+    }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
